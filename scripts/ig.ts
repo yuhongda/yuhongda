64148f0ae6e109f37a20e3e19d485f6d5de80e93
yuhongda <silverage.y@gmail.com>
2024-04-01 12:44:21 +0800

    fix: thumbnail_url

--- scripts/ig.ts
+++ scripts/ig.ts
@@ -18,6 +18,7 @@ async function fetchInstagram() {
 		let posts = [];
 		const top3 = json.data.slice(0, 9);
 		for (let i = 0; i < top3.length; i++) {
+			if (i === 0) continue;
 			console.log(`Fetching ${i + 1} of ${top3.length}`)
 			const response = await fetch(
 				`https://graph.instagram.com/${top3[i].id}?fields=media_url,thumbnail_url&access_token=${process.env.IG_TOKEN}}`,
@@ -32,7 +33,7 @@ async function fetchInstagram() {
 			const d = (await response.json()) as any;
 
 			// resize image to 50% of original size and convert to base64
-			const arrayBuffer = await fetch(d.media_url).then((res) => res.arrayBuffer());
+			const arrayBuffer = await fetch(d.thumbnail_url || d.media_url).then((res) => res.arrayBuffer());
 			const imageBuffer = Buffer.from(arrayBuffer);
 			const metadata = (await sharp(imageBuffer).metadata()) as any;
 			const resizedImageBuffer = await sharp(imageBuffer)
