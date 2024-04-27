5cb12a2125640e7f04d2fcc3d60765403be90206
yuhongda <silverage.y@gmail.com>
2024-04-27 16:19:07 +0800

    refactor: not resize for thumbnail

--- scripts/ig.ts
+++ scripts/ig.ts
@@ -18,7 +18,7 @@ async function fetchInstagram() {
 		let posts = [];
 		const top3 = json.data.slice(0, 9);
 		for (let i = 0; i < top3.length; i++) {
-			console.log(`Fetching ${i + 1} of ${top3.length}`)
+			console.log(`Fetching ${i + 1} of ${top3.length}`);
 			const response = await fetch(
 				`https://graph.instagram.com/${top3[i].id}?fields=media_url,thumbnail_url&access_token=${process.env.IG_TOKEN}}`,
 				{
@@ -30,13 +30,13 @@ async function fetchInstagram() {
 				}
 			);
 			const d = (await response.json()) as any;
-
+			const isThumbnail = d.thumbnail_url && !d.media_url;
 			// resize image to 50% of original size and convert to base64
 			const arrayBuffer = await fetch(d.thumbnail_url || d.media_url).then((res) => res.arrayBuffer());
 			const imageBuffer = Buffer.from(arrayBuffer);
 			const metadata = (await sharp(imageBuffer).metadata()) as any;
 			const resizedImageBuffer = await sharp(imageBuffer)
-				.resize({ width: Math.round(metadata.width / 4) })
+				.resize({ width: isThumbnail ? metadata.width : Math.round(metadata.width / 4) })
 				.toBuffer();
 			const base64 = resizedImageBuffer.toString('base64');
 
@@ -47,7 +47,7 @@ async function fetchInstagram() {
 		}
 
 		await writeFile('src/ig.json', JSON.stringify(posts));
-		console.log('Done')
+		console.log('Done');
 	}
 }
 
